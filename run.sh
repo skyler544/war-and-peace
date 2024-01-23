a86d84a0995206ff9c999962acdf77e663a527b8
Skyler Mayfield <skyler544@gmail.com>
2024-01-23 18:40:47 +0100

    add lparallel dependency to run script

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 sbcl --noinform \
      --eval '(ql:quickload :str :silent t)' \
+     --eval '(ql:quickload :lparallel :silent t)' \
      --load 'war-and-peace.lisp' \
      --eval '(output-categorization)' \
      --quit
