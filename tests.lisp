3fe86c4bdfa8df25df8265751e8b29b15aa912be
Skyler Mayfield <skyler544@gmail.com>
2024-01-28 14:11:31 +0100

    more simple assertions

--- tests.lisp
+++ tests.lisp
@@ -1,3 +1,6 @@
+(fiveam:test can-read-file
+  (fiveam:is (read-file "tests.lisp")))
+
 (fiveam:test can-read-book
   (fiveam:is (read-book)))
 
@@ -6,5 +9,11 @@
 
 (fiveam:test can-peace-terms
   (fiveam:is (read-peace-terms)))
+
+(fiveam:test can-split-string-into-words
+  (fiveam:is (equal '("foo" "bar") (tokenize-line "foo...bar"))))
+
+(fiveam:test can-split-text-into-lists-of-words
+  (fiveam:is (equal '(("foo" "bar")) (tokenize-text '("foo...bar")))))
 
 
